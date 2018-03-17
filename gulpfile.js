b881b9e8bc24a12aeeba3ea12d22f82291c60f95
uruskyi <uruskyi@gmail.com>
2018-03-17 07:42:21 +0200

    add gulp browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    watch = require('gulp-watch');
-    // browserSync = require('browser-sync');
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync');
 
     gulp.task('less', function () {
-        return gulp.src('app/less/main.less')
+        return gulp.src('app/less/**/*.less')
             .pipe(less())
             .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload({stream: true}))
     });
 
-    // gulp.task('browser-sync', function() {	// Створюємо таск browser-sync
-    //     browserSync({						// Виконуємо browser Sync
-    //         server: {						// Визначаємо параметри сервера
-    //             baseDir: 'app'				// Директорія для сервера - app
-    //         },
-    //         notify: false					// Відключаємо повідомлення зовнішнього ресурсу
-    //     });
-    // });
+    gulp.task('browser-sync', function() {	// Створюємо таск browser-sync
+        browserSync({						// Виконуємо browser Sync
+            server: {						// Визначаємо параметри сервера
+                baseDir: 'app'				// Директорія для сервера - app
+            },
+            notify: false					// Відключаємо повідомлення зовнішнього ресурсу
+        });
+    });
 
-    gulp.task('watch', function() {
-        gulp.watch('app/less/**/*.less', ['less']); // Нагляд за less файлами, де через кому вказується масив тасків, що будуть виконуватись
-        // Нагляд за іншими типами файлів
+    gulp.task('watch', ['browser-sync', 'less'], function() {
+        gulp.watch('app/less/**/*.less', ['less']);			// Нагляд за less файлами у папці less
+        gulp.watch('app/*.html', browserSync.reload);		// Нагляд за HTML файлами у корені проекта
+        gulp.watch('app/js/**/*.js', browserSync.reload);	// Нагляд за JS файлами у папці js
     });
