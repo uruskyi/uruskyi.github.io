b2e6ba2008bea476b317c4f01a3a8875e0f57b69
uruskyi <uruskyi@gmail.com>
2018-03-17 06:30:19 +0200

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    watch = require('gulp-watch');
+    // browserSync = require('browser-sync');
 
-gulp.task('less', function () {
-    return gulp.src('app/less/main.less')
-        .pipe(less())
-        .pipe(gulp.dest('app/css'))
-});
+    gulp.task('less', function () {
+        return gulp.src('app/less/main.less')
+            .pipe(less())
+            .pipe(gulp.dest('app/css'))
+    });
+
+    // gulp.task('browser-sync', function() {	// Створюємо таск browser-sync
+    //     browserSync({						// Виконуємо browser Sync
+    //         server: {						// Визначаємо параметри сервера
+    //             baseDir: 'app'				// Директорія для сервера - app
+    //         },
+    //         notify: false					// Відключаємо повідомлення зовнішнього ресурсу
+    //     });
+    // });
+
+    gulp.task('watch', function() {
+        gulp.watch('app/less/**/*.less', ['less']); // Нагляд за less файлами, де через кому вказується масив тасків, що будуть виконуватись
+        // Нагляд за іншими типами файлів
+    });
